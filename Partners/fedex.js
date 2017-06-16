13366423c2d9db5d876692b66f107703ca7bdfbf
akk144 <akanshagrawal25@gmail.com>
2017-06-16 11:56:29 +0530

    Change service type in fedex

--- Partners/fedex.js
+++ Partners/fedex.js
@@ -126,7 +126,7 @@ module.exports = Template.extend('FedEx', {
 		  RequestedShipment: {
 		    ShipTimestamp: new Date(date.getTime() + (24*60*60*1000)).toISOString(),
 		    DropoffType: 'REGULAR_PICKUP',
-		    ServiceType: (inp.is_cod) ? 'STANDARD_OVERNIGHT' : 'PRIORITY_OVERNIGHT',
+		    ServiceType: 'STANDARD_OVERNIGHT',
 		    PackagingType: 'YOUR_PACKAGING',
 		    TotalWeight : [{
 		    	Units : 'KG',
@@ -289,7 +289,7 @@ module.exports = Template.extend('FedEx', {
 		  RequestedShipment: {
 		    ShipTimestamp: new Date(date.getTime() + (24*60*60*1000)).toISOString(),
 		    DropoffType: 'REGULAR_PICKUP',
-		    ServiceType: 'PRIORITY_OVERNIGHT',
+		    ServiceType: 'STANDARD_OVERNIGHT',
 		    PackagingType: 'YOUR_PACKAGING',
 		    TotalWeight : [{
 		    	Units : 'KG',
