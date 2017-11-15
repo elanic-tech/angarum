37c2fb873552c54a674e54b6b04006a384d54a14
Pragyan Tripathi <pntripathi9417@gmail.com>
2017-11-15 14:21:14 +0530

    phone number shouldn't be empty

--- Partners/fedex.js
+++ Partners/fedex.js
@@ -119,10 +119,10 @@ module.exports = Template.extend('FedEx', {
 		var date = new Date();
         var from_mobile_number = inp.from_mobile_number;
         var to_mobile_number = inp.to_mobile_number;
-        if(inp.order_type === 'forward_p2p') {
-            inp.from_mobile_number = '';
-            inp.to_mobile_number = '';
-        }
+        // if(inp.order_type === 'forward_p2p') {
+        //     inp.from_mobile_number = '';
+        //     inp.to_mobile_number = '';
+        // }
 		inp.from_address = inp.from_address.replace(/\s\s+/g, ' ');
 		inp.to_address = inp.to_address.replace(/\s\s+/g, ' ');
 		var from_street_line_1 = inp.from_address.substring(0,35);
