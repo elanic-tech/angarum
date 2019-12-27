c89558cd96bff8eb5e493cfc8306ee8a6d665c26
Himanshu <himanshu@sharechat.co>
2019-12-27 20:14:58 +0530

    add rapid logistics partner

--- Partners/rapid.js
+++ Partners/rapid.js
@@ -17,11 +17,11 @@ module.exports = Template.extend('Rapid', {
     },
 
     order: function(params, cb) {
-	var url = `${host}/api/createpackage`;
+	var url = `${host}/api/createpackage.php`;
 	// Check out Order schema file for more information.
 
   const inp = params.get();
-  if(_.isEmpty(inp.from_address_line_1)) { 
+  if(_.isEmpty(inp.from_address_line_1)) {
     inp.from_address_line_1 = inp.from_address;
   }
   if(_.isEmpty(inp.to_address_line_1)) {
@@ -29,7 +29,7 @@ module.exports = Template.extend('Rapid', {
   }
   let req = {};
 
-  if (inp.order_type === "pickup" || inp.order_type==="return_pickup") {
+  if (inp.order_type === "pickup") {
     req = {
       client,
       token,
@@ -43,7 +43,7 @@ module.exports = Template.extend('Rapid', {
       country: inp.from_country,
       phone: inp.from_mobile_number,
       weight: 0.4,
-      mode: "reverse",
+      mode: imp.is_cod? 'cod' : 'prepaid',
       ret_add: inp.to_address_line_1 + inp.to_address_line_2,
       ship_pin: inp.to_pin_code,
       ship_phone: inp.to_mobile_number,
@@ -116,7 +116,7 @@ module.exports = Template.extend('Rapid', {
 
     single_tracking_status: function (params, cb) {
       const awb = params.get().awb_number;
-      const url = `${host}/api/track.php?client=${client}&token=${token}&waybill=${params.get().awb_number}`;
+      const url = get_tracking_url(params.get().awb_number);
       const headers = {};
       params.out_map({
         "err": "error"
@@ -160,4 +160,72 @@ module.exports = Template.extend('Rapid', {
 	return this.post_req(url, params, cb);
     },
 
+  // pickup: function(params, cb) {
+  //   var url = "/api/pickup.php";
+  //   params.map(["to_be_omitted_1", "to_be_omitted_2"], {
+  //     "from_mapping_1" : "to_mapping_1",
+  //     "from_mapping_2" : "to_mapping_2",
+  //   }, function(inp) {
+  //     return _.extend({
+  //       "token": token,
+  //       "client": client,
+  //     }, inp);
+  //   });
+  //
+  //   params.out_map({}, function(out) {
+  //     out.success = !Boolean(out.err);
+  //     return out;
+  //   });
+  //
+  //   return this.post_req(url, params, cb);
+  // },
+
+  pickup: function(params, cb) {
+    var url = `${host}/api/pickup.php`;
+    // Check out Order schema file for more information.
+
+    var date = new Date(params.date);
+    var difference = date.getTime() - Date.now().getTime();
+    var day = Math.floor(difference / (1000 * 60 * 60 * 24));
+    if(_.isEmpty(params.from_address_line_1)) {
+      params.from_address_line_1 = params.from_address;
+    }
+    if(_.isEmpty(inp.to_address_line_1)) {
+      params.to_address_line_1 = params.to_address;
+    }
+    let req = {
+        client,
+        token,
+        day,
+        address: params.from_address_line_1,
+        pincode: params.from_pin_code,
+        name: params.name,
+        shipments: params.quantity,
+        phone: params.from_mobile_number,
+      };
+
+    const postReq = unirest.post(url);
+    postReq.header('Content-Type', 'application/x-www-form-urlencoded');
+
+    for (let key in req) {
+      postReq.send(`${key}=${req[key]}`);
+    }
+
+    postReq.end((response) => {
+      const body = _.get(response, "body");
+      if (_.isEmpty(body) || _.get(response, "body.Pickup", []).filter((obj) => obj.status === 'Error').length !== 0) {
+        params.set({
+          success: false,
+          err: _.get(response, _.get(response, "body.Pickup", []).filter((obj) => obj.status === 'Error').map(obj => obj.remarks).join('|'), "Rapid Unknown Error"),
+        });
+      } else {
+        params.set({
+          success: true,
+          err: null,
+          details: body
+        });
+      }
+      return cb(response, params);
+    });
+  },
 });
